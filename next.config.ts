427c4a9006da1938b4430a178140b9acfdba8dee
David Biga <biga.david@gmail.com>
2025-02-11 19:00:26 +0900

    export as static

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     // TODO: Remove this because it should not be used for production, only being used for components from shadcn checking
